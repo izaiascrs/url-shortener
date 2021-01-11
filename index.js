4b83b7030d24ad5fd5be86feb09514c9f217ab69
izaiascrs <izaiascrs@gmail.com>
2021-01-11 15:28:09 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.json({ok: true})
+})
+
+// Define Routes
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
